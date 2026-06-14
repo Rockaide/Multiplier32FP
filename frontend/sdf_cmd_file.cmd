10f513d68860872f3e0d5f4038a79766bb0faf23
Filipe Eismann <54189571+FilipeEismann@users.noreply.github.com>
2026-06-14 18:01:38 -0300

    Delete frontend/sdf_cmd_file.cmd

--- frontend/sdf_cmd_file.cmd
+++
@@ -1,6 +0,0 @@
-COMPILED_SDF_FILE = "/home/ufsm00051/filipe.eismann/projetos/Multiplier32FP/backend/layout/deliverables/multiplier32FP_worst_368_0/multiplier32FP.sdf.X",
-SCOPE = multiplier32FP_vect_tb.DUV,
-LOG_FILE = "sdf.log",
-MTM_CONTROL = "MAXIMUM",
-SCALE_FACTORS = "1.0:1.0:1.0",
-SCALE_TYPE = "FROM_MTM";
